2dee94f28b7963c054f2897cadcf740180539667
Mike Eng <mike.eng@adhocteam.us>
2020-12-20 14:58:53 -0500

    checking each word against dictionary

--- wp-content/themes/neve-child/js/spellcheck.js
+++ wp-content/themes/neve-child/js/spellcheck.js
@@ -2,11 +2,21 @@ jQuery(document).ready(function($){
   var dictionary = new Typo("en_US", false, false, { dictionaryPath: neve_child_stylesheet_directory['stylesheet_directory_uri']+"/js/vendor/typo/dictionaries" });
   var comment_raw = '';
   var comment_stripped = '';
+  var words = [];
+  var is_spelled_correctly;
   $("#comment").on("input", function(){
         comment_raw = ($(this).val());
         comment_stripped = comment_raw.replace(/[^\w\s]|_/g, "").replace(/\n/g, " ").replace(/\s+/g, " ");
+        words = comment_stripped.split(' ');
+        /*
         console.log('comment raw = '+comment_raw);
         console.log('comment stripped = '+comment_stripped);
+        console.log(words);
+        */
+        $.each(words, function(index, item){
+          is_spelled_correctly = dictionary.check(this);
+          console.log(index+' = '+is_spelled_correctly);
+        })
   });
   /*
   var is_spelled_correctly = dictionary.check("mispelled");
