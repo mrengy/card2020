4f0399d1f9289cc8b1ca44e1de3e3c68ad63caad
Mike Eng <mike.eng@adhocteam.us>
2020-12-19 21:29:00 -0500

    loading dictionary files with stylesheet path

--- wp-content/themes/neve-child/js/spellcheck.js
+++ wp-content/themes/neve-child/js/spellcheck.js
@@ -1,3 +1,3 @@
 jQuery(document).ready(function($){
-  var dictionary = new Typo("en_US", false, false, { dictionaryPath: "vendor/typo/dictionaries" });
+  var dictionary = new Typo("en_US", false, false, { dictionaryPath: neve_child_stylesheet_directory['stylesheet_directory_uri']+"/js/vendor/typo/dictionaries" });
 });
