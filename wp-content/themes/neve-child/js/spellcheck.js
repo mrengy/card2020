d783b9db00a4be0de11aaa05e7cae246351cbc9d
Mike Eng <mike.eng@adhocteam.us>
2020-12-20 14:07:08 -0500

    start of stripping non alphanumeric characters from comment textarea

--- wp-content/themes/neve-child/js/spellcheck.js
+++ wp-content/themes/neve-child/js/spellcheck.js
@@ -1,3 +1,16 @@
 jQuery(document).ready(function($){
   var dictionary = new Typo("en_US", false, false, { dictionaryPath: neve_child_stylesheet_directory['stylesheet_directory_uri']+"/js/vendor/typo/dictionaries" });
+  var comment_raw = '';
+  var comment_stripped = '';
+  $("#comment").on("input", function(){
+        comment_raw = ($(this).val());
+        //console.log(comment_raw);
+        comment_stripped = JSON.stringify(comment_raw).replace(/[^a-z\d ]/i, '');
+        console.log('comment stripped = '+comment_stripped);
+        console.log('comment raw = '+comment_raw);
+  });
+  /*
+  var is_spelled_correctly = dictionary.check("mispelled");
+  console.log(is_spelled_correctly);
+  */
 });
