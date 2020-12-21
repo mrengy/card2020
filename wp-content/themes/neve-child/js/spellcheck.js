1353abba21da31b8e3af85fa7513b7bcd0dbfa9e
Mike Eng <mike.eng@adhocteam.us>
2020-12-21 14:45:27 -0500

    moving variable definitions inside input function

--- wp-content/themes/neve-child/js/spellcheck.js
+++ wp-content/themes/neve-child/js/spellcheck.js
@@ -1,12 +1,10 @@
 jQuery(document).ready(function($){
   var dictionary = new Typo("en_US", false, false, { dictionaryPath: neve_child_stylesheet_directory['stylesheet_directory_uri']+"/js/vendor/typo/dictionaries" });
-  var comment_raw = '';
-  var comment_stripped = '';
-  var words = [];
-  var is_spelled_correctly;
   $("#comment").on("input", function(){
-        comment_raw = ($(this).val());
-        comment_stripped = comment_raw.replace(/[^\w\s]|_/g, " ").replace(/\n/g, " ").replace(/\s+/g, " ");
+        var comment_raw = ($(this).val());
+        var comment_stripped = comment_raw.replace(/[^\w\s]|_/g, " ").replace(/\n/g, " ").replace(/\s+/g, " ");
+        var is_spelled_correctly;
+        var words = [];
         words = comment_stripped.split(' ');
         /*
         console.log('comment raw = '+comment_raw);
