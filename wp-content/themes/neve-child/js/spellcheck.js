42cf22902cc4e9eb5cb31de41633b8fbdf044793
Mike Eng <mike.eng@adhocteam.us>
2020-12-21 15:24:08 -0500

    json checkpoint

--- wp-content/themes/neve-child/js/spellcheck.js
+++ wp-content/themes/neve-child/js/spellcheck.js
@@ -1,19 +1,35 @@
 jQuery(document).ready(function($){
   var dictionary = new Typo("en_US", false, false, { dictionaryPath: neve_child_stylesheet_directory['stylesheet_directory_uri']+"/js/vendor/typo/dictionaries" });
+
+  var vocab = $.getJSON(neve_child_stylesheet_directory['stylesheet_directory_uri']+'/js/vocab.json', function(obj) {
+      console.log(obj);
+  });
+
   $("#comment").on("input", function(){
-        var comment_raw = ($(this).val());
-        var comment_stripped = comment_raw.replace(/[^\w\s]|_/g, " ").replace(/\n/g, " ").replace(/\s+/g, " ");
-        var is_spelled_correctly;
-        var words = [];
-        words = comment_stripped.split(' ');
+      var comment_raw = ($(this).val());
+      var comment_stripped = comment_raw.replace(/[^\w\s]|_/g, " ").replace(/\n/g, " ").replace(/\s+/g, " ");
+      var words = [];
+      words = comment_stripped.split(' ');
+      /*
+      console.log('comment raw = '+comment_raw);
+      console.log('comment stripped = '+comment_stripped);
+      console.log(words);
+      */
+      $.each(words, function(index, item){
+        var is_in_vocab = false;
+        for (var i = 0; i < vocab.word.length; ++i){
+          var thisWord = vocab.word[i].name;
+          //console.log(thisWord);
+          if(thisWord=this){
+            is_in_vocab = true;
+            break;
+          }
+        }
+        console.log(index+' = '+is_in_vocab);
         /*
-        console.log('comment raw = '+comment_raw);
-        console.log('comment stripped = '+comment_stripped);
-        console.log(words);
+        is_spelled_correctly = dictionary.check(this);
+        console.log(index+' = '+is_spelled_correctly);
         */
-        $.each(words, function(index, item){
-          is_spelled_correctly = dictionary.check(this);
-          console.log(index+' = '+is_spelled_correctly);
-        })
+      })
   });
 });
