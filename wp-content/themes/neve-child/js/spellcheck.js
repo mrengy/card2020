ec39d64b2197733d1c4dfacd393d1e39d95dcb00
Mike Eng <mike.eng@adhocteam.us>
2020-12-22 14:04:43 -0500

    cleanup

--- wp-content/themes/neve-child/js/spellcheck.js
+++ wp-content/themes/neve-child/js/spellcheck.js
@@ -10,18 +10,10 @@ jQuery(document).ready(function($){
       var words = [];
       words = comment_stripped.split(' ');
       var incorrect_words = [];
-      /*
-      console.log('comment raw = '+comment_raw);
-      console.log('comment stripped = '+comment_stripped);
-      console.log(words);
-      */
       $.each(words, function(index, item){
         var is_in_vocab = false;
-        //console.log(vocab.responseJSON);
         for (var i = 0; i < vocab.responseJSON.word.length; ++i){
-
           var thisWord = vocab.responseJSON.word[i].name;
-          //console.log(thisWord);
           if(thisWord.toUpperCase()===this.toUpperCase()){
             is_in_vocab = true;
             break;
@@ -31,15 +23,9 @@ jQuery(document).ready(function($){
             incorrect_words.push(this);
           }
         }
-
         // show whether each number word in the comment is in the vocab
         //console.log(index+' = '+is_in_vocab);
-
         console.log(incorrect_words);
-        /*
-        is_spelled_correctly = dictionary.check(this);
-        console.log(index+' = '+is_spelled_correctly);
-        */
       })
   });
 });
