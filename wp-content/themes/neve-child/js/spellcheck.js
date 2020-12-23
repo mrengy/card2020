26c5c63782a411e31a9db151f1c8fe66586bc797
Mike Eng <mike.eng@adhocteam.us>
2020-12-22 22:38:02 -0500

    refactored

--- wp-content/themes/neve-child/js/spellcheck.js
+++ wp-content/themes/neve-child/js/spellcheck.js
@@ -4,32 +4,35 @@ jQuery(document).ready(function($){
 
   var incorrect_words = [];
   //check words input in comment field against vocab
-  $("#comment").on("input", function(){
-      var comment_raw = ($(this).val());
-      var comment_stripped = comment_raw.replace(/[^\w\s]|_/g, "").replace(/\n/g, " ").replace(/\s+/g, " ");
-      var words = [];
-      words = comment_stripped.split(' ');
-      //reset incorrect_words
-      incorrect_words = [];
+  function checkwords(){
 
-      $.each(words, function(index, item){
-        var is_in_vocab = false;
-        for (var i = 0; i < vocab.responseJSON.word.length; ++i){
-          var thisWord = vocab.responseJSON.word[i].name;
-          if(thisWord.toUpperCase()===this.toUpperCase()){
-            is_in_vocab = true;
-            break;
-          }
-          //when we are checking the last word in the vocab, if it doesn't match the word typed
-          else if(i == (vocab.responseJSON.word.length - 1) && thisWord.toUpperCase()!=this.toUpperCase()) {
-            incorrect_words.push(this);
-          }
+    var comment_raw = $(this).val();
+    var comment_stripped = comment_raw.replace(/[^\w\s]|_/g, "").replace(/\n/g, " ").replace(/\s+/g, " ");
+    var words = [];
+    words = comment_stripped.split(' ');
+    //reset incorrect_words
+    incorrect_words = [];
+
+    $.each(words, function(index, item){
+      var is_in_vocab = false;
+      for (var i = 0; i < vocab.responseJSON.word.length; ++i){
+        var thisWord = vocab.responseJSON.word[i].name;
+        if(thisWord.toUpperCase()===this.toUpperCase()){
+          is_in_vocab = true;
+          break;
         }
-        // show whether each number word in the comment is in the vocab
-        //console.log(index+' = '+is_in_vocab);
-        //console.log(incorrect_words);
-      })
-  });
+        //when we are checking the last word in the vocab, if it doesn't match the word typed
+        else if(i == (vocab.responseJSON.word.length - 1) && thisWord.toUpperCase()!=this.toUpperCase()) {
+          incorrect_words.push(this);
+        }
+      }
+      // show whether each number word in the comment is in the vocab
+      //console.log(index+' = '+is_in_vocab);
+      //console.log(incorrect_words);
+    })
+  };
+
+  $("#comment").on("input", checkwords);
 
   $('#commentform').submit(function(e){
     if (incorrect_words.length > 0){
