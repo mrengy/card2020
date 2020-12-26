f9a48c0f77a176bdce65143b27d03c411510c0e9
Mike Eng <mike.eng@adhocteam.us>
2020-12-26 15:54:10 -0500

    fixed json

--- wp-content/themes/neve-child/functions.php
+++ wp-content/themes/neve-child/functions.php
@@ -81,8 +81,6 @@ function neve_child_enqueue_scripts() {
       // This is where you run the code and display the output
       $vocab_file = file_get_contents( get_stylesheet_directory_uri().'/js/vocab.json' );
       $vocab_arr = json_decode($vocab_file, true);
-      console_log($vocab_file);
-      console_log($vocab_arr);
 
       //sorting words alphabetically
       $words_arr = [];
